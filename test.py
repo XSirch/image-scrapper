9456483427cf225211265666514c966ad92663c2
Christiano Leszkiewicz <69818038+XSirch@users.noreply.github.com>
2026-04-19 16:50:47 -0300

    chore: remove test files and pycache from repository

--- test.py
+++
@@ -1,7 +0,0 @@
-from scrapling.fetchers import StealthyFetcher
-url = 'https://www.carolbassi.com.br/vestido-irene-vinho-cassis-i470190104-2843/p'
-page = StealthyFetcher.fetch(url, headless=True)
-for img in page.css('img'):
-    src = img.attrib.get('data-zoom-image') or img.attrib.get('data-large') or img.attrib.get('data-src') or img.attrib.get('src') or img.attrib.get('data-original')
-    if src and 'I470' in src:
-        print('SRC:', src)
